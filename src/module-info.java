b57bd69836625f66751cdf02a4f58d67bd36c399
CloudAtlas2144 <paul.delage@utbm.fr>
2021-12-29 16:21:22 +0100

    update

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module KanagawaGame {
-    requires javafx.base;
-    requires javafx.graphics;
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens kanagawa;
-    opens kanagawa.views;
-}
